9acd7d759debedb21ceaee8a172a04937e051726
IgorMatviuk <igor11.11.9988@gmail.com>
2020-07-03 00:54:34 +0300

    Updates

---
+++ precache-manifest.21bcd61762c034a0089328d55708da6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b635a6822ca9c57e77adde6c43e4e5ba",
+    "url": "/github-pages-COVID-Ukraine-React/index.html"
+  },
+  {
+    "revision": "116f04a7962484983559",
+    "url": "/github-pages-COVID-Ukraine-React/static/css/main.99eaa828.chunk.css"
+  },
+  {
+    "revision": "fc0cc7a9e306eb653031",
+    "url": "/github-pages-COVID-Ukraine-React/static/js/2.b3046ac3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/github-pages-COVID-Ukraine-React/static/js/2.b3046ac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "116f04a7962484983559",
+    "url": "/github-pages-COVID-Ukraine-React/static/js/main.58bce7e6.chunk.js"
+  },
+  {
+    "revision": "fc1bdb8e4f62e9ee8489",
+    "url": "/github-pages-COVID-Ukraine-React/static/js/runtime-main.3a597ae1.js"
+  }
+]);
